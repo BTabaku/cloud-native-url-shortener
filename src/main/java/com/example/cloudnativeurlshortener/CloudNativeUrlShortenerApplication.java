b35328e9d814a493204109f6bedfc199b8cdba0d
BTabaku <baftit40@gmail.com>
2025-01-29 11:54:17 +0100

    added a structure

---
+++ src/main/java/com/example/cloudnativeurlshortener/CloudNativeUrlShortenerApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloudnativeurlshortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudNativeUrlShortenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudNativeUrlShortenerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cloudnativeurlshortener/CloudNativeUrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudnativeurlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudNativeUrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
